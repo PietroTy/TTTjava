d8457b613e51869909fa414e011def69415cafa0
Rodiza <rodiza082003@hotmail.com>
2024-12-11 19:17:43 -0300

    Controler para vitoriaO, mas nao funcional

---
+++ src/main/java/br/edu/ifsp/VitoriaO.java
@@ -0,0 +1,8 @@
+package br.edu.ifsp;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class VitoriaO {
+    
+}
--- src/main/java/br/edu/ifsp/VitoriaX.java
+++ src/main/java/br/edu/ifsp/VitoriaX.java
@@ -1,10 +1,13 @@
 package br.edu.ifsp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class VitoriaX {
 
     @FXML
-    private Text xNome;
+    private Text caixaDeTexto;
+
+
 }
